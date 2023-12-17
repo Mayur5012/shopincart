f7b6be87552e9ae28417412cc2ca971cd29ee3a5
Mayuresh Gawali <mayureshgawali9@gmail.com>
2023-12-17 11:44:29 +0530

    invoice temp changed

--- services/common.js
+++ services/common.js
@@ -6,7 +6,7 @@ let transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: 'bpay1009@gmail.com', // gmail
+    user: 'team.dripatindia@gmail.com', // gmail
     pass: process.env.MAIL_PASSWORD, // pass
   },
 });
@@ -31,7 +31,7 @@ exports.cookieExtractor = function (req) {
 
 exports.sendMail = async function ({to, subject, text, html}){
     let info = await transporter.sendMail({
-        from: '"DRIP@360" <bpay1009@gmail.com>', // sender address
+        from: '"DRIP@360" <team.dripatindia@gmail.com>', // sender address
         to,
         subject,
         text,
@@ -43,318 +43,224 @@ exports.sendMail = async function ({to, subject, text, html}){
 exports.invoiceTemplate = function(order){
 
  return (`<!DOCTYPE html>
-<html>
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Email Receipt</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style type="text/css">
-  /**
-   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
-   */
-  @media screen {
-    @font-face {
-      font-family: 'Source Sans Pro';
-      font-style: normal;
-      font-weight: 400;
-      src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
-    }
-
-    @font-face {
-      font-family: 'Source Sans Pro';
-      font-style: normal;
-      font-weight: 700;
-      src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
-    }
-  }
-
-  /**
-   * Avoid browser level font resizing.
-   * 1. Windows Mobile
-   * 2. iOS / OSX
-   */
-  body,
-  table,
-  td,
-  a {
-    -ms-text-size-adjust: 100%; /* 1 */
-    -webkit-text-size-adjust: 100%; /* 2 */
-  }
-
-  /**
-   * Remove extra space added to tables and cells in Outlook.
-   */
-  table,
-  td {
-    mso-table-rspace: 0pt;
-    mso-table-lspace: 0pt;
-  }
-
-  /**
-   * Better fluid images in Internet Explorer.
-   */
-  img {
-    -ms-interpolation-mode: bicubic;
-  }
-
-  /**
-   * Remove blue links for iOS devices.
-   */
-  a[x-apple-data-detectors] {
-    font-family: inherit !important;
-    font-size: inherit !important;
-    font-weight: inherit !important;
-    line-height: inherit !important;
-    color: inherit !important;
-    text-decoration: none !important;
-  }
-
-  /**
-   * Fix centering issues in Android 4.4.
-   */
-  div[style*="margin: 16px 0;"] {
-    margin: 0 !important;
-  }
-
-  body {
-    width: 100% !important;
-    height: 100% !important;
-    padding: 0 !important;
-    margin: 0 !important;
-  }
-
-  /**
-   * Collapse table borders to avoid space between cells.
-   */
-  table {
-    border-collapse: collapse !important;
-  }
-
-  a {
-    color: #1a82e2;
-  }
-
-  img {
-    height: auto;
-    line-height: 100%;
-    text-decoration: none;
-    border: 0;
-    outline: none;
-  }
-  </style>
-
-</head>
-<body style="background-color: #D2C7BA;">
-
-  <!-- start preheader -->
-  <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
-    A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
-  </div>
-  <!-- end preheader -->
-
-  <!-- start body -->
-  <table border="0" cellpadding="0" cellspacing="0" width="100%">
-
-    <!-- start logo -->
-    <tr>
-      <td align="center" bgcolor="#D2C7BA">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
-        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-          <tr>
-            <td align="center" valign="top" style="padding: 36px 24px;">
-              <a href="https://drip-360.vercel.app/" target="_blank" style="display: inline-block;">
-                <img src="logo_bl_tr.png" alt="DRIP@360" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
-              </a>
-            </td>
-          </tr>
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end logo -->
-
-    <!-- start hero -->
-    <tr>
-      <td align="center" bgcolor="#D2C7BA">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
-        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-          <tr>
-            <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-              <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Thank you for your order!</h1>
-            </td>
-          </tr>
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end hero -->
-
-    <!-- start copy block -->
-    <tr>
-      <td align="center" bgcolor="#D2C7BA">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
-        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-          <!-- start copy -->
-          <tr>
-            <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-              <p style="margin: 0;">Here is a summary of your recent order. If you have any questions or concerns about your order, please <a href="bpay1009@gmail.com">contact us</a>.</p>
-            </td>
-          </tr>
-          <!-- end copy -->
-
-          <!-- start receipt table -->
-          <tr>
-            <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-              <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                <tr>
-                  <td align="left" bgcolor="#D2C7BA" width="60%" style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"><strong>Order #</strong></td>
-                  <td align="left" bgcolor="#D2C7BA" width="20%" style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"><strong></strong></td>
-                  <td align="left" bgcolor="#D2C7BA" width="20%" style="padding: 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"><strong>${order.id}</strong></td>
-                </tr>
-                ${order.items.map(item=>`<tr>
-                  <td align="left" width="60%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">${item.product.title}</td>
-                  <td align="left" width="20%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">${item.quantity}</td>
-                  <td align="left" width="20%" style="padding: 6px 12px;font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">₹${Math.round(item.product.price*(1-item.product.discountPercentage/100),2)}</td>
-                </tr>`)
-
-                }
-               
-               
-                <tr>
-                  <td align="left" width="60%" style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>Total</strong></td>
-                  <td align="left" width="20%" style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>${order.totalItems}</strong></td>
-                  <td align="left" width="20%" style="padding: 12px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>₹${order.totalAmount}</strong></td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-          <!-- end reeipt table -->
-
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end copy block -->
-
-    <!-- start receipt address block -->
-    <tr>
-      <td align="center" bgcolor="#D2C7BA" valign="top" width="100%">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
-        <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-          <tr>
-            <td align="center" valign="top" style="font-size: 0; border-bottom: 3px solid #d4dadf">
-              <!--[if (gte mso 9)|(IE)]>
-              <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-              <tr>
-              <td align="left" valign="top" width="300">
-              <![endif]-->
-              <div style="display: inline-block; width: 100%; max-width: 50%; min-width: 240px; vertical-align: top;">
-                <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 300px;">
-                  <tr>
-                    <td align="left" valign="top" style="padding-bottom: 36px; padding-left: 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                      <p><strong>Delivery Address</strong></p>
-                      <p>${order.selectedAddress.name}<br>${order.selectedAddress.street}<br>${order.selectedAddress.city},${order.selectedAddress.state},${order.selectedAddress.pinCode}</p>
-                      <p>${order.selectedAddress.phone}</p>
-
-                      </td>
-                  </tr>
-                </table>
-              </div>
-              <!--[if (gte mso 9)|(IE)]>
-              </td>
-              <td align="left" valign="top" width="300">
-              <![endif]-->
-            
-              <!--[if (gte mso 9)|(IE)]>
-              </td>
-              </tr>
-              </table>
-              <![endif]-->
-            </td>
-          </tr>
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end receipt address block -->
-
-    <!-- start footer -->
-    <tr>
-      <td align="center" bgcolor="#D2C7BA" style="padding: 24px;">
-        <!--[if (gte mso 9)|(IE)]>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-        <tr>
-        <td align="center" valign="top" width="600">
-        <![endif]-->
-        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-
-          <!-- start permission -->
-          <tr>
-            <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">You received this email because we received a request for [type_of_action] for your account. If you didn't request [type_of_action] you can safely delete this email.</p>
-            </td>
-          </tr>
-          <!-- end permission -->
-
-          <!-- start unsubscribe -->
-          <tr>
-            <td align="center" bgcolor="#D2C7BA" style="padding: 12px 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 20px; color: #666;">
-              <p style="margin: 0;">To stop receiving these emails, you can <a href="https://sendgrid.com" target="_blank">unsubscribe</a> at any time.</p>
-              <p style="margin: 0;">Paste 1234 S. Broadway St. City, State 12345</p>
-            </td>
-          </tr>
-          <!-- end unsubscribe -->
-
-        </table>
-        <!--[if (gte mso 9)|(IE)]>
-        </td>
-        </tr>
-        </table>
-        <![endif]-->
-      </td>
-    </tr>
-    <!-- end footer -->
-
-  </table>
-  <!-- end body -->
-
-</body>
-</html>`
+ <html lang="en">
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+   <title>Email Receipt</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <style type="text/css">
+     @media screen {
+       @font-face {
+         font-family: 'Source Sans Pro';
+         font-style: normal;
+         font-weight: 400;
+         src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+       }
+ 
+       @font-face {
+         font-family: 'Source Sans Pro';
+         font-style: normal;
+         font-weight: 700;
+         src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+       }
+     }
+ 
+     body {
+       width: 100% !important;
+       height: 100% !important;
+       padding: 0 !important;
+       margin: 0 !important;
+       background-color: #f7f7f7;
+     }
+ 
+     table {
+       border-collapse: collapse !important;
+       width: 100% !important;
+     }
+ 
+     img {
+       max-width: 100%;
+       height: auto;
+       line-height: 100%;
+       text-decoration: none;
+       border: 0;
+       outline: none;
+       display: block;
+       margin: 0 auto;
+     }
+ 
+     a {
+       color: #3366cc;
+       text-decoration: none;
+     }
+ 
+     .preheader {
+       display: none;
+       max-width: 0;
+       max-height: 0;
+       overflow: hidden;
+       font-size: 1px;
+       line-height: 1px;
+       color: #fff;
+       opacity: 0;
+     }
+ 
+     /* Header */
+     .header {
+       background-color: #2c3e50;
+       padding: 20px;
+     }
+ 
+     .header img {
+       max-width: 100px;
+     }
+ 
+     .hero {
+       padding: 36px 24px 0;
+       font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+       border-top: 3px solid #d4dadf;
+       background-color: #ffffff;
+     }
+ 
+     .hero h1 {
+       margin: 0;
+       font-size: 32px;
+       font-weight: 700;
+       letter-spacing: -1px;
+       line-height: 48px;
+       color: #333333;
+     }
+ 
+     /* Content Block */
+     .content {
+       padding: 24px;
+       font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+       font-size: 16px;
+       line-height: 24px;
+       background-color: #ffffff;
+     }
+ 
+     .content p {
+       margin: 0;
+       color: #666666;
+     }
+ 
+     /* Receipt Table */
+     .receipt-table {
+       padding: 24px;
+       font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+       font-size: 16px;
+       line-height: 24px;
+       background-color: #ffffff;
+     }
+ 
+     .receipt-table strong {
+       font-weight: bold;
+     }
+ 
+     .receipt-table table {
+       width: 100%;
+     }
+ 
+     .receipt-table td {
+       padding: 12px;
+       border-top: 2px dashed #D2C7BA;
+       border-bottom: 2px dashed #D2C7BA;
+     }
+ 
+     /* Receipt Address Block */
+     .receipt-address {
+       font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+       font-size: 16px;
+       line-height: 24px;
+       background-color: #ffffff;
+       border-bottom: 3px solid #d4dadf;
+     }
+ 
+     .receipt-address p {
+       margin: 0;
+     }
+ 
+     /* Footer */
+     .footer {
+       padding: 24px;
+       font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+       font-size: 14px;
+       line-height: 20px;
+       color: white;
+       background-color: #2c3e50;
+     }
+   </style>
+ </head>
+ <body>
+ 
+   <div class="preheader">
+     A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
+   </div>
+ 
+   <!-- Header -->
+   <div class="header" style="text-align: center; background-color: #2c3e50; padding: 20px;">
+     <a href="https://dripat360.vercel.app/" target="_blank">
+       <img src="./logo_bl_tr.png" alt="DRIP@360" style="max-width: 100px;">
+     </a>
+   </div>
+ 
+   <!-- Hero -->
+   <div class="hero">
+     <h1>Thank you for your order!</h1>
+   </div>
+ 
+   <!-- Content Block -->
+   <div class="content">
+     <p>Here is a summary of your recent order. If you have any questions or concerns about your order, please <a href="mailto:team.dripatindia@gmail.com">contact us</a>.</p>
+   </div>
+ 
+   <!-- Receipt Table -->
+   <div class="receipt-table">
+     <table>
+       <tr>
+         <td width="60%"><strong>Order #</strong></td>
+         <td width="20%">Item Quantity</td>
+         <td width="20%"><strong>${order.id}</strong></td>
+       </tr>
+       ${order.items.map(item => `
+         <tr>
+           <td width="60%">${item.product.title}</td>
+           <td width="20%">${item.quantity}</td>
+           <td width="20%">₹${Math.round(item.product.price * (1 - item.product.discountPercentage / 100), 2)}</td>
+         </tr>
+       `)}
+       <tr>
+         <td width="60%" style="border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>Total</strong></td>
+         <td width="20%" style="border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>${order.totalItems}</strong></td>
+         <td width="20%" style="border-top: 2px dashed #D2C7BA; border-bottom: 2px dashed #D2C7BA;"><strong>₹${order.totalAmount}</strong></td>
+       </tr>
+     </table>
+   </div>
+ 
+   <!-- Receipt Address Block -->
+   <div class="receipt-address" style="border-bottom: 3px solid #d4dadf;">
+     <div style="display: inline-block; width: 100%; max-width: 50%; min-width: 240px; vertical-align: top;">
+       <table>
+         <tr>
+           <td style="padding-bottom: 36px; padding-left: 36px;">
+             <p><strong>Delivery Address</strong></p>
+             <p>${order.selectedAddress.name}<br>${order.selectedAddress.street}<br>${order.selectedAddress.city},${order.selectedAddress.state},${order.selectedAddress.pinCode}</p>
+             <p>${order.selectedAddress.phone}</p>
+           </td>
+         </tr>
+       </table>
+     </div>
+   </div>
+ 
+   <!-- Footer -->
+   <div class="footer">
+     <p>You received this email because we received a request for order from your account. If you didn't request the order, you can <a href="mailto:team.dripatindia@gmail.com">contact us</a>!</p>
+   </div>
+ 
+ </body>
+ </html>`
  )
 
 
