ca4a2d2526fe08b477834a6df8337a33c10dfe4d
Mayuresh Gawali <mayureshgawali9@gmail.com>
2024-01-24 20:48:16 +0530

    product now displayed at main page JAI SHREE RAM.

--- index.js
+++ index.js
@@ -95,14 +95,24 @@ server.use(
   })
 );
 server.use(express.json()); // to parse req.body
-server.use('/products', isAuth(), productsRouter.router);
+server.use('/products', productsRouter.router);
 // we can also use JWT token for client-only auth
-server.use('/categories', isAuth(), categoriesRouter.router);
-server.use('/brands', isAuth(), brandsRouter.router);
+server.use('/categories', categoriesRouter.router);
+server.use('/brands', brandsRouter.router);
 server.use('/users', isAuth(), usersRouter.router);
 server.use('/auth', authRouter.router);
 server.use('/cart', isAuth(), cartRouter.router);
 server.use('/orders', isAuth(), ordersRouter.router);
+
+
+// server.use('/products', isAuth(), productsRouter.router);
+// // we can also use JWT token for client-only auth
+// server.use('/categories', isAuth(), categoriesRouter.router);
+// server.use('/brands', isAuth(), brandsRouter.router);
+// server.use('/users', isAuth(), usersRouter.router);
+// server.use('/auth', authRouter.router);
+// server.use('/cart', isAuth(), cartRouter.router);
+// server.use('/orders', isAuth(), ordersRouter.router);
 
 // this line we add to make react router work in case of other routes doesnt match
 server.get('*', (req, res) =>
